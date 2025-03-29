65918069848f029620a34d4218535e05e00206de
friendy <friendykaliman@gmail.com>
2025-03-29 14:07:18 +0700

    Add files via upload

---
+++ app/survey/page.tsx
@@ -0,0 +1,8 @@
+// app/promo/route.ts
+import { redirect } from 'next/navigation'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  // Redirect directly to the Google Form
+  redirect("https://docs.google.com/forms/d/e/1FAIpQLScM2CFULJen8j9OlYQXCMiufV_FqKjIuDn7XytBZ6FDY5wXQQ/viewform?usp=dialog")
+}
