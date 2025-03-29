a96938e5d130909f5c99a58f30bcbe40ffdd7ce3
friendy <friendykaliman@gmail.com>
2025-03-29 14:06:27 +0700

    Update route.ts

--- app/promo/route.ts
+++ app/promo/route.ts
@@ -4,5 +4,5 @@ import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   // Redirect directly to the Google Form
-  redirect("https://docs.google.com/forms/d/e/1FAIpQLScM2CFULJen8j9OlYQXCMiufV_FqKjIuDn7XytBZ6FDY5wXQQ/viewform")
+  redirect("https://drive.google.com/file/d/1nXU_GULnF2psCRmXMQSe-dC8W4SDB68T/view?usp=sharing")
 }
